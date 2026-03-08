2717b8efa9cdb03f464c22f5bfed67d58b505dc2
Saif <saif.abbas0192@gmail.com>
2026-03-08 18:35:08 +0000

    Added an map showing off the crime and where it takes place the most

---
+++ dashbord.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import ast
+
+
+st.title("London Crime Dashbord")
+df = pd.read_csv("data_crime.csv")
+
+st.metric("total crimes", len(df))
+crime_counts = df["category"].value_counts()
+st.bar_chart(crime_counts)
+print(df["location"].head())
+
+df["lat"] =df["location"].apply(lambda x: float(ast.literal_eval(x)["latitude"]))
+df["lon"] =df["location"].apply(lambda x: float(ast.literal_eval(x)["longitude"]))
+st.map(df, latitude="lat", longitude="lon", zoom=10)
+
+print(df["lat"].head())
+print(df["lon"].head())
